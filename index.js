923807e2028567ba7a11ce284973518d243c86d1
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-02-18 13:14:06 -0800

    fix path to async-airtable

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import './asyncAirtable-beta.min.js';
+import './lib/asyncAirtable-beta.min.js';
 const asyncAirtable = new AsyncAirtable("keyyCSADD61J0D4wQ", "appGzEQkD3QySIDvj", { baseURL: "https://api.vacfind.org/v0" });//, { ...CONFIG }
 
 
