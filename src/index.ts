10ac119fc2b788d6cfc62ad42b1f2a6816acebfe
Shravan Kumar Karnati <shravan.kumar063@gmail.com>
2021-03-07 20:35:55 -0800

    refactoring
    1) Removed the baseUrl parameter from the async airtable instantiation based on the documentation. Works fine now.
    2) Removed the persits methods - I believe its better to persist the results on the client side. Since the API can also be run on a browser-less environment, in which case this persit methods throws an error.
    3) The loadLinks method now runs a Promise which in-turn returns the results(links) when fulfilled.

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,18 @@
-import AsyncAirtable = require("asyncairtable");
-import { AirtableRecord, QueryObject } from "asyncairtable/lib/@types";
+import AsyncAirtable from 'asyncairtable';
+import { QueryObject, AirtableRecord } from 'asyncairtable/lib/@types';
 
-const asyncAirtable = new AsyncAirtable("keyyCSADD61J0D4wQ", "appGzEQkD3QySIDvj", { baseURL: "https://api.vacfind.org/v0" });//, { ...CONFIG }
+export const asyncAirtable = new AsyncAirtable(
+  'keyyCSADD61J0D4wQ',
+  'appGzEQkD3QySIDvj'
+);
 
-
-export const helloWorld = () => {
-	console.log("Hello World");
-}
-
-export var loadLinks = async (filterOpts:QueryObject) => {
-
-	// let allrecords = []
-
-	return asyncAirtable.select('links', {
-		sort: [
-			{ field: 'group', direction: 'asc' }
-		],
-		where: filterOpts
-	}).then((results: AirtableRecord[]) => persistToStorage(results))
+export let loadLinks = async (
+  filterOpts: QueryObject
+): Promise<AirtableRecord[]> => {
+  return asyncAirtable
+    .select('links', {
+      sort: [{ field: 'group', direction: 'asc' }],
+      where: filterOpts
+    })
+    .then((results: AirtableRecord[]) => results);
 };
-
-const persistToStorage = (results: AirtableRecord[]) => {
-	console.log("persisting to storage")
-	if (results) {
-		localStorage.setItem("vacFind-links", JSON.stringify(results));
-	}
-	return results
-}
-
-export const retrieveFromStorage = ():AirtableRecord[] => {
-	console.log("retrieving from storage")
-	const storedValue = localStorage.getItem("vacFind-links");
-	if (storedValue){
-		return JSON.parse(storedValue);
-	} else {
-		return [];
-	}
-	
-}
