32ac79c3dc33835654d81746b19f645771706796
Shravan Kumar Karnati <shravan.kumar063@gmail.com>
2021-03-07 20:37:07 -0800

    tests
    Wrote a couple simple tests for testing the index file

---
+++ src/__tests__/airtableInstance.test.ts
@@ -0,0 +1,7 @@
+import { asyncAirtable } from "./../index";
+
+describe("Tests for Airtable instance", () => {
+  test("init. of airtable instance", () => {
+    expect(asyncAirtable.base).not.toBeNull();
+  });
+});
